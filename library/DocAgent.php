00957849d9d9045612a63d7e9daad05f717b7617
laughingman94 <42205593+laughingman94@users.noreply.github.com>
2019-11-13 12:39:29 +0100

    módosított docAgent

--- library/DocAgent.php
+++ library/DocAgent.php
@@ -5,8 +5,6 @@ class DocAgent {
 
     public function __construct()
     {
-		header("Location:index.php");
-		die();
     }
 
     private function _getDocPath($fileId) {
@@ -19,7 +17,8 @@ class DocAgent {
 
     public function getDoc($fileId) {
         $fileName = $this->_getDocPath($fileId);
-        return file_get_contents($fileName);
+		return false;
+        //return file_get_contents($fileName);
     }
 
     public function storeDoc($fileId, $content) {
@@ -72,7 +71,7 @@ class DocAgent {
     }
 
     public function showDocBinary($id, $code) {
-        error_reporting(0);
+        /*error_reporting(0);
         if (!$fileData = sql_fetch_array(sql_query("select * from dokumentumok where id=? and kod=?",array($id, $code)))) {
             die("error 2");
         }
@@ -100,7 +99,7 @@ class DocAgent {
         if ($fileData["tipus"]=="xlsx") header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
         if ($fileData["tipus"]=="jpg" || $fileData["tipus"]=="jpeg") header("Content-Type: image/jpeg");
         if ($fileData["tipus"]=="png") header("Content-Type: image/png");
-        echo $file;
+        echo $file;*/
         die();
     }
 
