bb0b40814a745f1c8024451ed51d37341922f25e
marciCloud <m.gergely9409@gmail.com>
2020-04-15 17:09:35 +0200

    kicsi szöveg módosítás

--- library/pages/RemoteBookingPage.php
+++ library/pages/RemoteBookingPage.php
@@ -194,6 +194,14 @@ class RemoteBookingPage extends CorePage{
 		//Képfeltöltés:
 		$html.= "<table><tr><td>";
         $html.= "<div style='font-size:16px'>Kérem, csatoljon 2-4 jó minőségű fényképet a tünetektől.</div>";
+		
+		$html.= "<div style='font-size:16px'>";
+		$html.= "-	Készítsen távoli képet, pl.: egész végtag, törzs, arc stb., hogy a bőrtünetek egyben jól láthatók legyenek!<br>";
+		$html.= "-	Mellékeljen továbbá TÖBB közeli képet, melyeken az egyes bőrtünetek jól láthatók!<br>";
+		$html.= "-	Kérjük ügyeljen a fényviszonyokra és jó fotóminőségre.<br>";
+		$html.= "-	Amennyiben a tünetek több testrészt is érintenek, kérjük mellékeljen egész testes képet is.";
+		$html.= "</div>";
+		
         $html.= "<div class='upload-btn-wrapper'><a href='#' class='upbtn newbutton'>{$webText["dokumentumfeltoltese"]}</a><input type='file' id='paciensfile' name='paciensfile[]' multiple /></div><img id='paciensloader' style='display:none;opacity:.5;height:30px;margin-left:10px;' src='/images/loading.svg' />";
         $html.= "</td></tr>";
         $html.= "<tr><td><div id='paciensfilediv'>".$this->utils->showPaciensFiles()."</div></td></tr>";
