bd54c855c208312655bb1786e1f612c12f7f7c64
laughingman94 <42205593+laughingman94@users.noreply.github.com>
2020-12-10 12:45:36 +0100

    elveszettnek hitt funkcióm :P

--- library/Utils.php
+++ library/Utils.php
@@ -948,4 +948,54 @@ class Utils {
         return htmlentities(trim(str_replace("\n\n","\n",str_replace("<","\n<", $result))));
     }
 
+    public function create_zip( $files = array(), $destination = '', $removeContainers = NULL, $overwrite = false ) {
+        //if the zip file already exists and overwrite is false, return false
+        if( file_exists( $destination ) && !$overwrite ) { return false; }
+        //vars
+        $valid_files = array();
+        //if files were passed in...
+        if( is_array( $files )) {
+            //cycle through each file
+            foreach( $files as $file ) {
+                //make sure the file exists
+                if( file_exists( $file )) {
+                    $valid_files[] = $file;
+                }
+            }
+        }
+        //if we have good files...
+        if( count( $valid_files )) {
+            //create the archive
+            $zip = new ZipArchive();
+            if( $zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true ) {
+                return false;
+            }
+            //add the files
+            foreach( $valid_files as $file ) {
+                if($removeContainers != NULL)
+                {
+                    $onlyFile = explode( "/", $file );
+                    $zip->addFile( $file, $onlyFile[$removeContainers] );
+                }
+                else
+                {
+                    $zip->addFile($file, $file);
+                }
+            }
+            //debug
+            //echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+            //close the zip -- done!
+            $zip->close();
+            
+            //system("zip -P $password $destination $destination");
+            
+            //check to make sure the file exists
+            return file_exists( $destination );
+        }
+        else
+        {
+            return false;
+        }
+    }
+
 }
