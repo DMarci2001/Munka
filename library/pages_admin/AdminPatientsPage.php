7df3ff1e4be1160d4eb908b55bfc3d3a792d0c31
jns <jns@jns.hu>
2022-01-21 17:30:53 +0100

    egyéb design

--- library/pages_admin/AdminPatientsPage.php
+++ library/pages_admin/AdminPatientsPage.php
@@ -293,7 +293,7 @@ class AdminPatientsPage extends AdminCorePage {
                     echo "<td valign='top'><div class='{$tc}'>{$rowf["helyszin"]}</div></td>";
                     echo "<td valign='top'><div class='{$tc}'>{$rowf["szurestipus"]}</div></td>";
                     echo "<td valign='top'><div class='{$tc}'>{$rowf["orvos"]}</div></td>";
-                    echo "<td valign='top'><div class='{$tc}'><a target='laborkero' href='index.php?page=laborkero&fid={$rowf["id"]}&p={$rowf["pass"]}'>+ Labor lelet</a></div></td>";
+                    echo "<td valign='top'><div class='{$tc}'><a target='laborkero' href='index.php?page=laborkero&fid={$rowf["id"]}&p={$rowf["pass"]}'>+ Laborkérő</a></div></td>";
                     echo "</tr>";
 
                     echo "<tr id='bmegj{$rowf["id"]}' style='display:none;'><td colspan='7'><div style='display:inline-block;background:#eee;padding:5px;margin:0px 0px 10px 10px;'>".nl2br($rowf["beutalomegj"])."</div></td></tr>";
