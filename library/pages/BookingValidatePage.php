087806bd2995f532b7ea02e13ab4a3724c721e4f
laughingman94 <42205593+laughingman94@users.noreply.github.com>
2024-09-17 15:43:41 +0200

    középre igazítás fix

--- library/pages/BookingValidatePage.php
+++ library/pages/BookingValidatePage.php
@@ -188,7 +188,7 @@ class BookingValidatePage extends CorePage {
                     <br/>
                     ";
                 }
-                echo "<h2 style=\"text-align:center\">{$webText["sikeresidopontreg"]}</h2>";
+                echo "<h2 ".(CompanyService::isSuzukiGHC()?"style=\"text-align:center\"":"").">{$webText["sikeresidopontreg"]}</h2>";
                 echo "{$webText["kedves"]} ".$this->foglalasData["nev"]."!<br>
                 <br>
                 {$successText}
