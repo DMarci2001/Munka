4fc59773fc5a6cb9f6dc02536f8ffcdcfe97edbb
laughingman94 <42205593+laughingman94@users.noreply.github.com>
2025-01-23 15:35:26 +0100

    suzuki stat módosítások

--- library/pages_admin/DailyStat/AdminSuzukiStatPage.php
+++ library/pages_admin/DailyStat/AdminSuzukiStatPage.php
@@ -15,6 +15,7 @@ class AdminSuzukiStatPage extends AdminCorePage
     private  $endDate = "0000-00-00";
     //private $utils;
     private const cegId = 892;
+    private $icons;
 
 
 
@@ -29,26 +30,30 @@ class AdminSuzukiStatPage extends AdminCorePage
             return;
         }
 
+        $facodes = sql_query("SELECT id,facode,megnev FROM szurestipusok WHERE facode<>''")->fetchAll(PDO::FETCH_ASSOC);
+        foreach ($facodes as $facode) {
+            $this->icons[] = ["id" => $facode["id"], "icon" => $facode["facode"], "name" => $facode["megnev"]];
+            //$this->$icons[$facode["id"]] = $facode["facode"];
+        }
+
         $this->bookings = $this->fetch_suzuki_bookings();
 
         if (isset($_GET["download-excel"])) {
             //$data = $this->fetch_suzuki_ghc_registrations();
             $excelService = new ExcelService();
             $array = [];
-            foreach($this->bookings as $booking){
-              
+            foreach ($this->bookings as $booking) {
+
                 $array[] = array(
-                    "Teljesnév"=>$booking["nev"],
-                    "Szül. dátum"=>str_replace("-",".",$booking["szuldatum"]),
-                    "TAJ szám"=>$booking["taj"],
-                    "Időpont"=>str_replace("-",".",$booking["datum"]),
-                    "Csomag"=>$booking["megnev"],
-                    "Eljött"=>$booking["eljott"],
+                    "Teljesnév" => $booking["nev"],
+                    "Szül. dátum" => str_replace("-", ".", $booking["szuldatum"]),
+                    "TAJ szám" => $booking["taj"],
+                    "Időpont" => str_replace("-", ".", $booking["datum"]),
+                    "Csomag" => $booking["megnev"],
+                    "Eljött" => $booking["eljott"],
                 );
             }
 
-
-            
             $excelService->generateXlsxFromArray($array, "A", "N", []);
             $excelService->setFileName("suzuki_menedzserek_" . date("Ymdhis") . ".xlsx");
             $excelService->outputSpreadSheet();
@@ -92,8 +97,8 @@ class AdminSuzukiStatPage extends AdminCorePage
         //$html .= "    </li>";
         $html .= "</ul>";
         $html .= "<div class='tab-content' id='myTabContent'>";
-        $html .= "    <div class='tab-pane pt-3 ps-3 fade show active' id='send-dm-tab-pane' role='tabpanel' aria-labelledby='profile-tab' tabindex='0'>".$this->show_suzuki_stat_data_table($data)."</div>";
-        $html .= "    <div class='tab-pane pt-3 ps-3 fade' id='previous-send-list-tab-pane' role='tabpanel' aria-labelledby='home-tab' tabindex='0'>".$this->show_booked_patient_list()."</div>";
+        $html .= "    <div class='tab-pane pt-3 ps-3 fade show active' id='send-dm-tab-pane' role='tabpanel' aria-labelledby='profile-tab' tabindex='0'>" . $this->show_suzuki_stat_data_table($data) . "</div>";
+        $html .= "    <div class='tab-pane pt-3 ps-3 fade' id='previous-send-list-tab-pane' role='tabpanel' aria-labelledby='home-tab' tabindex='0'>" . $this->show_booked_patient_list() . "</div>";
         //$html .= "    <div class='tab-pane pt-3 ps-3 fade' id='recipient-list-tab-pane' role='tabpanel' aria-labelledby='profile-tab' tabindex='0'>...</div>";
         $html .= "</div>";
 
@@ -146,13 +151,13 @@ class AdminSuzukiStatPage extends AdminCorePage
     /**
      * Ez a funkció meg keresi azon vizsgálatokat a csomagból, amire már nincs foglalható időpont.
      */
-    private function search_for_bottleneck($packages, $date, $beo)
+    private function search_for_bottleneck($packages, $date)
     {
         $dayCode = date("N", strtotime($date));
-        $returnArray = [];
+        $returnArray = $mixed = [];
         foreach ($packages as $package) {
             $schedules = [];
-            echo "A vizsgált csomag: {$package}<br>";
+            //echo "A vizsgált csomag: {$package}<br>";
             //Csomaghoz tartozó vizsgálatok
             $types = sql_query(
                 "SELECT szk.szurestipusid,sz.megnev FROM szurescsomagok_kapcs szk
@@ -163,47 +168,75 @@ class AdminSuzukiStatPage extends AdminCorePage
 
             //Összes lefoglalt időpont az adott napra
             $bookings = sql_query(
-                "SELECT id,szurestipusid,orvosassigned,datum FROM foglalasok WHERE szurestipusid IN(".implode(",", array_column($types, "szurestipusid")).") AND datum BETWEEN ? AND ?",
+                "SELECT id,szurestipusid,orvosassigned,datum FROM foglalasok WHERE helyszinid=1 AND szurestipusid IN(" . implode(",", array_column($types, "szurestipusid")) . ") AND datum BETWEEN ? AND ?",
                 [$date . " 00:00:00", $date . " 23:59:59"]
             )->fetchAll(PDO::FETCH_ASSOC);
 
             //Vizsgálatokhoz tartozó beosztások
             foreach ($types as $key => $value) {
                 $schedules[$value["szurestipusid"]] = sql_query(
-                    "SELECT tol as startTime,ig as endTime,binterval,orvosid,beonap,nap 
+                    "SELECT id,tol as startTime,ig as endTime,binterval,orvosid,beonap,nap 
                      FROM orvos_beosztas_new 
                      WHERE (nap=? OR beonap=?) AND INSTR(tipusok,?) AND aktiv=1 AND helyszinid=1 AND INSTR(beocegek,?)",
-                    [$dayCode, $date, "|{$value["szurestipusid"]}|","|892|"]
+                    [$dayCode, $date, "|{$value["szurestipusid"]}|", "|892|"]
                 )->fetchAll(PDO::FETCH_ASSOC);
             }
-          
+
             //Orvosonként kell definiálni a beosztásokat...
-            foreach($schedules as $examination=>$schedule_properties){
-                echo "Vizsgálat: {$examination}<br>";
+            foreach ($schedules as $examination => $schedule_properties) {
+                if (!isset($schedules[$examination]["availability"]["free"])) {
+                    $schedules[$examination]["availability"]["free"] = 0;
+                }
+                
                 //A beosztásokból kinézem az orvosok id-jait
-                $doctors = array_unique(array_column($schedule_properties,"orvosid"));
-                foreach($doctors as $doctor){
+                $doctors = array_unique(array_column($schedule_properties, "orvosid"));
+                if($date=="2025-01-24" && $examination==10){
+                    //echo "Orvosok:<br>";
+                    //$this->debug_array($doctors);
+                }
+                foreach ($doctors as $doctor) {
                     //Megkeresem az orvosokhoz tartozó beosztásokat
-                    $doctor_schedules = array_keys(array_column($schedule_properties,"orvosid"),$doctor);
+                    $doctor_schedules = array_keys(array_column($schedule_properties, "orvosid"), $doctor);
                     $capacity = 0;
                     //Kikalkulálom a beosztások adatai alapján a lehetséges időpontok számát.
-                    foreach($doctor_schedules as $schedule){
-                        $capacity = ($capacity+$this->calc_capacity($schedule_properties[$schedule]));
+                    foreach ($doctor_schedules as $schedule) {
+                        $capacity = ($capacity + $this->calc_capacity($schedule_properties[$schedule]));
                     }
                     $schedules[$examination]["availability"][$doctor]["capacity"] = $capacity;
                     //Kikeresem az orvoshoz tartozó foglalásokat és letárolom.
-                    $bookedTimes = array_keys(array_column($bookings,"orvosassigned"),$doctor);
+                    $bookedTimes = array_keys(array_column($bookings, "orvosassigned"), $doctor);
+                    if($date=="2025-01-24" && $examination==10){
+                        //echo "Lefoglalt időpontok: {$examination} - {$doctor}<br>";
+                        foreach($bookedTimes as $key){
+                            //$this->debug_array($bookings[$key]);
+                        }
+                    }
                     $schedules[$examination]["availability"][$doctor]["booked"] = count($bookedTimes);
-                    echo "vizsgáló orvos: {$doctor}<br>";
-                    if($doctor==1019){
-                        echo "lefoglalt időpontok: <br>";
-                        //$this->debug_array($bookedTimes);
+                    $schedules[$examination]["availability"][$doctor]["free"] = ($capacity - count($bookedTimes));
+                    $schedules[$examination]["availability"]["free"] = ($schedules[$examination]["availability"]["free"] + ($capacity - count($bookedTimes)));
+                    if($date=="2025-01-24" && $examination==10){
+                        //echo "Vizsgálat: {$examination} - {$doctor}<br>";
+                        //$this->debug_array($schedules[$examination]);
+                    }
+                    $returnArray[$package]["availability"][$examination]["free"] = $schedules[$examination]["availability"]["free"];
+                    if (!isset($mixed[$examination])) {
+                        if($returnArray[$package]["availability"][$examination]["free"]<0){
+                            $mixed[$examination] = 0;
+                        }else{
+                            $mixed[$examination] = $returnArray[$package]["availability"][$examination]["free"];
+                        }
+                        
                     }
                 }
-                $this->debug_array($schedules[$examination]);
-            } 
-            return;
+                
+                //$this->debug_array($schedules[$examination]);
+            }
         }
+        if($date=="2025-01-24" && $examination==10){
+            //$this->debug_array($mixed);
+        }
+        //$this->debug_array($mixed);
+        return $mixed;
     }
 
     private function _calc_capacity($date, $typeIds)
@@ -349,11 +382,12 @@ class AdminSuzukiStatPage extends AdminCorePage
         return $data;
     }
 
-    private function show_booked_patient_list(){
+    private function show_booked_patient_list()
+    {
         $html = "";
         $html .= "<div class='container-xxl mx-3'>";
         $html .= "<div class=\"h6\">Összes foglalás " . date("Y.m.d", strtotime($this->startDate)) . " óta: <strong>" . count($this->bookings) . "db</strong></div>";
-        $html .= "<div class=\"h6\">Vizsgálaton résztvettek száma " . date("Y.m.d", strtotime($this->startDate)) . "  óta: <strong>".count(array_keys(array_column($this->bookings,"eljott"),1))."db</strong></div>";
+        $html .= "<div class=\"h6\">Vizsgálaton résztvettek száma " . date("Y.m.d", strtotime($this->startDate)) . "  óta: <strong>" . count(array_keys(array_column($this->bookings, "eljott"), 1)) . "db</strong></div>";
         $html .= "<div class=\"h6\">Lista letöltése:&nbsp;<a target='_blank' href='?page=suzukistat&download-excel'><img src='https://{$_SERVER["HTTP_HOST"]}/admin/images/excel_icon.png' height='40px'></a></div>";
         $html .= "<table class=\"table table-striped\">";
         $html .= "   <thead>";
@@ -368,15 +402,15 @@ class AdminSuzukiStatPage extends AdminCorePage
         $html .= "       </tr>";
         $html .= "   </thead>";
         $html .= "   <tbody>";
-        foreach($this->bookings as $key=>$appointment){
+        foreach ($this->bookings as $key => $appointment) {
             $html .= "<tr>";
             $html .= "<th class=\"text-center\" scope=\"row\">" . ($key + 1) . ".</th>";
             $html .= "<td class=\"text-center\">{$appointment["nev"]}</td>";
-            $html .= "<td class=\"text-center\">".str_replace("-",".",$appointment["szuldatum"])."</td>";
+            $html .= "<td class=\"text-center\">" . str_replace("-", ".", $appointment["szuldatum"]) . "</td>";
             $html .= "<td class=\"text-center\">{$appointment["taj"]}</td>";
-            $html .= "<td class=\"text-center\">".str_replace("-",".",$appointment["datum"])."</td>";
+            $html .= "<td class=\"text-center\">" . str_replace("-", ".", $appointment["datum"]) . "</td>";
             $html .= "<td class=\"text-center\">{$appointment["megnev"]}</td>";
-            $html .= "<td class=\"text-center\">".($appointment["eljott"]==1?"Eljött":"Nem jött el")."</td>";
+            $html .= "<td class=\"text-center\">" . ($appointment["eljott"] == 1 ? "Eljött" : "Nem jött el") . "</td>";
             $html .= "</tr>";
         }
         $html .= "   </tbody>";
@@ -398,7 +432,7 @@ class AdminSuzukiStatPage extends AdminCorePage
         $webText = $lang->webText;
         $html .= "<div class='container-xxl mx-3'>";
         $html .= "<div class=\"h6\">Összes foglalás " . date("Y.m.d", strtotime($this->startDate)) . " óta: <strong>" . count($this->bookings) . "db</strong></div>";
-        $html .= "<div class=\"h6\">Vizsgálaton résztvettek száma " . date("Y.m.d", strtotime($this->startDate)) . "  óta: <strong>".count(array_keys(array_column($this->bookings,"eljott"),1))."db</strong></div>";
+        $html .= "<div class=\"h6\">Vizsgálaton résztvettek száma " . date("Y.m.d", strtotime($this->startDate)) . "  óta: <strong>" . count(array_keys(array_column($this->bookings, "eljott"), 1)) . "db</strong></div>";
         $html .= "<table class=\"table table-striped\">";
         $html .= "   <thead>";
         $html .= "       <tr class=\"h5\">";
@@ -406,22 +440,27 @@ class AdminSuzukiStatPage extends AdminCorePage
         $html .= "       <th class=\"text-center\" scope=\"col\"><i class='fa-regular fa-calendar-days'></i></th>";
         $html .= "       <th class=\"text-center\" scope=\"col\"><i class='fa-solid fa-4'></i><i class='fa-solid fa-5'></i><i class='fa-solid fa-plus'></i></th>";
         $html .= "       <th class=\"text-center\" scope=\"col\"><i class='fa-solid fa-4'></i><i class='fa-solid fa-5'></i><i class='fa-solid fa-minus'></i></th>";
+        $html .= "       <th class=\"text-center\" scope=\"col\"><i class='fa-solid fa-triangle-exclamation'></i></th>";
         $html .= "       <th class=\"text-center\" scope=\"col\"><i class='fa-solid fa-mars'></i></th>";
         $html .= "       <th class=\"text-center\" scope=\"col\"><i class='fa-solid fa-venus'></i></th>";
         $html .= "       </tr>";
         $html .= "   </thead>";
         $html .= "   <tbody>";
         foreach ($data as $key => $value) {
+            if(strtotime($value["booking_date"])<strtotime("today")){
+                continue;
+            }
             $plus45 = ["capacity" => 0, "times" => ["overall" => 0, "male" => 0, "female" => 0]];
             $minus45 = ["capacity" => 0, "times" => ["overall" => 0, "male" => 0, "female" => 0]];
             $male = $female = 0;
+            $noTimeLeft = "";
             foreach ($value["params"] as $beo) {
                 $plus = $minus = 0; //Ez azért kell, mert multiplikálja az időpontokat a szűréstípusokkal xd remélhetőleg a jövőben ez nem szül problémát majd.
                 foreach ($beo["types"] as $type) {
                     //echo "beosztás: ".$type." - {$value["booking_date"]}<br>";
                     if (in_array($type, [219, 222]) && $minus == 0) {
                         $minus++;
-                        if($value["booking_date"]=="2025-01-22"){
+                        if ($value["booking_date"] == "2025-01-22") {
                             //echo $value["booking_date"]."<br>";
                             //$this->search_for_bottleneck([219, 222], $value["booking_date"], $beo);
                             //return;
@@ -432,10 +471,30 @@ class AdminSuzukiStatPage extends AdminCorePage
                     }
                     if (in_array($type, [220, 221]) && $plus == 0) {
                         $plus++;
+                        //$plus45["bottlenecks"] = $this->search_for_bottleneck([220, 221], $value["booking_date"], $beo);
                         $plus45["capacity"] = ($plus45["capacity"] + $this->calc_capacity($beo));
                         $plus45["times"] = $this->get_bookedTimes($value["booking_date"], $beo, $plus45["times"]);
                     }
                 }
+            }
+
+            $bottlenecks = $this->search_for_bottleneck([219, 220, 221, 222], $value["booking_date"]);
+            
+            $issues = array_keys($bottlenecks, 0);
+            /*if($value["booking_date"]=="2025-01-24"){
+                echo "összes vizsgálat:<br>";
+                $this->debug_array($bottlenecks);
+                echo "elfogyott vizsgálatok:<br>";
+                $this->debug_array($issues);
+            }*/
+           
+            //$this->debug_array($this->icons);
+            foreach ($issues as $examination) {
+                if ($noTimeLeft != "") {
+                    $noTimeLeft .= "&nbsp;";
+                }
+                $key = array_search($examination,array_column($this->icons,"id"));
+                $noTimeLeft .= "<span title='{$this->icons[$key]["name"]}'>{$this->icons[$key]["icon"]}</span>";
             }
 
             $male = ($minus45["times"]["male"] + $plus45["times"]["male"]);
@@ -447,14 +506,21 @@ class AdminSuzukiStatPage extends AdminCorePage
             $html .= "<td class=\"text-center\">" . str_replace("-", ".", $value["booking_date"]) . ", " . ucfirst($webText["hetnap"][date("N", strtotime($value["booking_date"]))]) . "</td>";
             $html .= "<td class=\"text-center\">{$plus45["times"]["overall"]}/{$plus45["capacity"]}</td>";
             $html .= "<td class=\"text-center\">{$minus45["times"]["overall"]}/{$minus45["capacity"]}</td>";
+            $html .= "<td class=\"text-center\">{$noTimeLeft}</td>";
             $html .= "<td class=\"text-center\">" . ($male > 0 ? $male . "db" : " - ") . "</td>";
             $html .= "<td class=\"text-center\">" . ($female > 0 ? $female . "db" : " - ") . "</td>";
             $html .= "</tr>";
+            if ($value["booking_date"] == "2025-01-22") {
+                //break;
+                //echo $value["booking_date"]."<br>";
+                //$this->search_for_bottleneck([219, 222], $value["booking_date"], $beo);
+                //return;
+            }
         }
         $html .= "   </tbody>";
         $html .= "</table>";
         $html .= "</div>";
-         
+
 
         return $html;
     }
