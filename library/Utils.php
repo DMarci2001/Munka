f1353ebd7891ecccd93ad11516635fd10d7a9c8c
laughingman94 <42205593+laughingman94@users.noreply.github.com>
2026-04-14 12:09:09 +0200

    robots kezelés update

--- library/Utils.php
+++ library/Utils.php
@@ -330,8 +330,11 @@ class Utils
             $favicon = "/images/logo_{$subdomain}.png";
         }
 
+        if($_SESSION["helyszindata"]["id"]!=Booking_Constants::DEFAULT_COMPANY_ID){
+            $htmlout .= "<meta name='robots' content='noindex, nofollow'>";
+        }
+        
         $htmlout .= "<link rel='shortcut icon' type='image/png' href='{$favicon}' />";
-        $htmlout .= "<meta name='robots' content='noindex, nofollow'>";
         $htmlout .= '<script type="text/javascript" src="/js/jquery/jquery-3.7.1.min.js"></script>';
         $htmlout .= '<script type="text/javascript" src="/js/jquery/jquery-ui.js"></script>';
         $htmlout .= '<link href="/js/air-datepicker-master/dist/css/datepicker.css" rel="stylesheet" type="text/css">';
