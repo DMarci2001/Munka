523efece5298374f7b365a9813c7bd4414ae27cd
petrovszky.gergo <grgdevelop@gmail.com>
2026-07-07 10:39:48 +0200

    Publikus beosztás nézet: rendelés megjegyzésének megjelenítése a kártyán

--- library/pages_workschedule/WorkScheduleService.php
+++ library/pages_workschedule/WorkScheduleService.php
@@ -254,7 +254,11 @@ class WorkScheduleService {
         $res = sql_query(
             "SELECT
                 DATE(m.datumfrom) AS datum,
-                m.datumfrom, m.datumto, m.tipusid, m.workerid, m.megj,
+                m.datumfrom, m.datumto, m.tipusid, m.workerid,
+                COALESCE(
+                    (SELECT dm.megj FROM schedule_datum_megj dm WHERE dm.tipusid = m.tipusid AND dm.datum = DATE(m.datumfrom)),
+                    t.megj
+                ) AS megj,
                 t.megnev AS tipusnev, t.kulso, t.kiszallas, t.roleid AS tipusroleid, t.cim, t.rendelo,
                 (
                     SELECT GROUP_CONCAT(
